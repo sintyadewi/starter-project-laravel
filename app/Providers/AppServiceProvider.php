8d10cf0f402c059d8aba51ae4a28c7b21be1e035
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-19 10:56:07 +0800

    Registering notification & polymorph aliases

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\ServiceProvider;
+use NotificationChannels\Fcm\FcmChannel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +29,23 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->resolving(LengthAwarePaginator::class, function ($paginator) {
             return $paginator->withQueryString();
+        });
+
+        $this->polymorphAliases();
+        $this->registerCustomChannel();
+    }
+
+    protected function polymorphAliases(): void
+    {
+        Relation::morphMap([
+            'user' => \App\Modules\Membership\Models\User::class,
+        ]);
+    }
+
+    protected function registerCustomChannel(): void
+    {
+        Notification::extend('firebase', function ($app) {
+            return FcmChannel::class;
         });
     }
 }
