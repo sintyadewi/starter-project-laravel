2d4f8d20edffc6b01c33ecc81d9b73360d3e19f6
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-23 13:25:24 +0800

    Define channel method

--- app/Http/Controllers/Admin/Notification/FirebasePushNotificationController.php
+++ app/Http/Controllers/Admin/Notification/FirebasePushNotificationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Notification;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Membership\Models\User;
+use App\Modules\Membership\Notifications\SendPushNotification;
 use Illuminate\Http\Request;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Laravel\Firebase\Facades\Firebase;
@@ -29,5 +31,23 @@ class FirebasePushNotificationController extends Controller
         ]);
 
         $this->notification->send($message);
+    }
+
+    public function channel()
+    {
+        // $user = User::with('fcmTokens')->find(2);
+
+        // foreach ($user->fcmTokens as $fcmToken) {
+        //     $user->addFcmToken($fcmToken);
+        // }
+
+
+        $user = User::find(2);
+
+        $user->notify(new SendPushNotification(
+            'Account Activated',
+            'Your account has been activated.',
+            'https://picsum.photos/200',
+        ));
     }
 }
