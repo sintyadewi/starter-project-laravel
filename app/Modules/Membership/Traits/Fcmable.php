0b72f20932ac6bcc4bd60ee199bf7d736a707edf
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-23 13:23:33 +0800

    Manage multiple fcm token

--- app/Modules/Membership/Traits/Fcmable.php
+++ app/Modules/Membership/Traits/Fcmable.php
@@ -3,23 +3,40 @@
 namespace App\Modules\Membership\Traits;
 
 use App\Modules\Membership\Models\FcmToken;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait Fcmable
 {
-    protected ?FcmToken $fcmToken;
+    protected ?FcmToken $singleFcmToken;
+    protected ?Collection $multipleFcmToken;
 
     public function currentFcmToken(): ?FcmToken
     {
-        return $this->fcmToken;
+        return $this->singleFcmToken;
     }
 
     public function withFcmToken(?FcmToken $fcmToken): static
     {
-        $this->fcmToken = $fcmToken;
+        $this->singleFcmToken = $fcmToken;
 
         return $this;
+    }
+
+    public function initializeHasFcmTokens(): void
+    {
+        $this->multipleFcmToken = new Collection();
+    }
+
+    public function addFcmToken(FcmToken $fcmToken): void
+    {
+        $this->multipleFcmToken->add($fcmToken);
+    }
+
+    public function getFcmTokens(): Collection
+    {
+        return $this->multipleFcmToken;
     }
 
     /**
