76a3ddc8647934dd1a1745e2d1ac68b89548d177
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-24 15:30:01 +0800

    Create news migration & model

---
+++ app/Modules/News/Models/News.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Modules\News\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class News extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+}
