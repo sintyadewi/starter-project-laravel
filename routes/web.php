bed2ad9bd1202441e0d43443e3961f9ede8bac99
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-25 16:47:18 +0800

    Define route to export pdf

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Export\ExportDomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('welcome');
 });
+
+Route::get('/export-dom', [ExportDomController::class, 'export']);
