380f5803420c98e6311e3774d8ae5b2bbcc94282
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-25 16:47:28 +0800

    Implement export pdf using dompdf

---
+++ app/Http/Controllers/Admin/Export/ExportDomController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Export;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\Export\ExportDomResource;
+use App\Modules\Membership\Models\User;
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class ExportDomController extends Controller
+{
+    public function export()
+    {
+        $users = User::all();
+
+        $userData = ExportDomResource::collection($users);
+
+        $pdf = Pdf::loadView('tableView', [
+            'users' => $userData,
+        ]);
+
+        return $pdf->stream('invoice.pdf');
+    }
+}
