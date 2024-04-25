c50e572fa7618cd1b98d0a42ef59e97e35d0ac0a
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-25 10:21:44 +0800

    Try to apply user filter

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Membership\Filters\UserFilter;
 use App\Modules\Membership\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,13 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $users = User::filter($request)->get();
+        // using old timedoor/filter package
+        // $users = User::filter($request)->get();
+
+        // using latest timedoor/laravel-filter package
+        $users = User::applyFilter(UserFilter::class, [
+            'exclude' => ['email']
+        ])->get();
 
         dd($users);
     }
