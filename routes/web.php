b2e3506d1f68eacc5a35a3c63be48ae2541c70b0
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-05-02 13:12:10 +0800

    Define route to export excel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('welcome');
 });
+
+Route::get('users/export', [UserController::class, 'exportExcel']);
