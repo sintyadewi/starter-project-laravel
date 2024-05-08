c670fb863e9564f57595e8ea0c5a6974eb183f7a
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-05-08 17:33:08 +0800

    Implement excel export for large dataset

--- app/Http/Controllers/Admin/Sample/SampleController.php
+++ app/Http/Controllers/Admin/Sample/SampleController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin\Sample;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\ExportLargeExcel;
 use App\Modules\Sample\Exports\SampleOrderExportByCollection;
+use App\Modules\Sample\Exports\SampleOrderExportByGenerator;
+use App\Modules\Sample\Exports\SampleOrderExportByQuery;
+use App\Modules\Sample\Exports\SampleOrderExportByView;
 use App\Modules\Sample\Models\SampleOrder;
 use App\Modules\Sample\Models\SampleOrderStatus;
 use App\Modules\Sample\Models\SamplePackage;
@@ -12,18 +16,19 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class SampleController extends Controller
 {
-    // public function exportByView(): BinaryFileResponse
-    // {
-    //     return Excel::download(new UserExportByView, 'userView.xlsx');
-    // }
-
-    // public function exportByQuery(): BinaryFileResponse
-    // {
-    //     return Excel::download(new UserExportByQuery, 'userQuery.xlsx');
-    // }
-
-    public function orderCollection(): BinaryFileResponse
+    public function exportExcel()
     {
-        return Excel::download(new SampleOrderExportByCollection, 'order-collection.xlsx');
+        $orders = SampleOrder::query()->with([
+            'samplePackage:id,price,hour,minute',
+            'sampleMember:id,name,phone,gender,birth_date,is_vip',
+            'sampleTherapist:id,name,alias_name,gender,address,birth_date,rating,trainer',
+            'sampleOrderStatus:id,status'
+        ])->lazy();
+
+        return Excel::download(new SampleOrderExportByCollection($orders), 'orders.xlsx');
+
+        // return Excel::queue(new SampleOrderExportByCollection, 'test.xlsx');
+        // return Excel::download(new SampleOrderExportByView, 'test.xlsx');
+
     }
 }
--- app/Modules/Sample/Exports/SampleOrderExportByCollection.php
+++ app/Modules/Sample/Exports/SampleOrderExportByCollection.php
@@ -3,42 +3,69 @@
 namespace App\Modules\Sample\Exports;
 
 use App\Modules\Sample\Models\SampleOrder;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithCustomChunkSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Shared\StringHelper;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class SampleOrderExportByCollection implements FromCollection, ShouldAutoSize, WithHeadings, WithMapping, WithStyles
+class SampleOrderExportByCollection implements
+    FromCollection,
+    // ShouldAutoSize,
+    // ShouldQueue,
+    // WithCustomChunkSize,
+    WithHeadings,
+    WithMapping,
+    WithColumnFormatting
+// WithStyles
 {
     use Exportable;
 
-    public function __construct()
+    public function __construct(public $orders)
     {
         //do something
     }
 
     public function collection()
     {
-        return SampleOrder::query()->with([
-            'samplePackage:id,price',
-            'sampleMember:id,name,phone',
-            'sampleTherapist:id,name',
-            'sampleOrderStatus:id,status'
-        ])->get();
+        return $this->orders;
     }
 
     public function headings(): array
     {
         return [
             'Order ID',
+            'Payment Type',
             'Duration',
             'Price',
+            'Start Time',
+            'End Time',
+            'Note',
+            'Platform',
+            'Package Price',
+            'Package Hour',
+            'Package Minute',
             'Member Name',
             'Member Phone',
-            'Therapist',
+            'Member Gender',
+            'Member Birth Date',
+            'Member Vip',
+            'Therapist Name',
+            'Therapist Alias Name',
+            'Therapist Gender',
+            'Therapist Address',
+            'Therapist Birth Date',
+            'Therapist Rating',
+            'Therapist Trainer',
             'Status',
         ];
     }
@@ -47,17 +74,49 @@ class SampleOrderExportByCollection implements FromCollection, ShouldAutoSize, W
     {
         return [
             $order->order_id,
+            $order->payment_type,
             $order->duration,
+            $order->price,
+            // Date::dateTimeToExcel($order->start_time),
+            // Date::dateTimeToExcel($order->end_time),
+            $order->start_time->format('d F Y'),
+            $order->end_time->format('d F Y'),
+            $order->formatted_note,
+            $order->platform,
             $order->samplePackage->price,
+            $order->samplePackage->hour,
+            $order->samplePackage->minute,
             $order->sampleMember->name,
             $order->sampleMember->phone,
+            $order->sampleMember->formatted_gender,
+            $order->sampleMember?->birth_date->format('Y/m/d'),
+            $order->sampleMember->formatted_is_vip,
             $order->sampleTherapist?->name,
+            $order->sampleTherapist?->alias_name,
+            $order->sampleTherapist?->formatted_gender,
+            $order->sampleTherapist?->address,
+            $order->sampleTherapist?->birth_date->format('d-m-Y'),
+            $order->sampleTherapist?->rating,
+            $order->sampleTherapist?->trainer,
             $order->sampleOrderStatus->status,
         ];
     }
 
-    public function styles(Worksheet $sheet)
+    // public function chunkSize(): int
+    // {
+    //     return 500;
+    // }
+
+    public function columnFormats(): array
     {
-        // $sheet->getStyle('B2')->getFont()->setBold(true);
+        return [
+            // 'E' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'M' => NumberFormat::FORMAT_TEXT,
+        ];
     }
+
+    // public function styles(Worksheet $sheet)
+    // {
+    //     // $sheet->getStyle('B2')->getFont()->setBold(true);
+    // }
 }
