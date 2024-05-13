0b98308b54d53e943ecc1b43432de7a50645ef67
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-05-13 14:18:29 +0800

    Update order status enum

--- app/Modules/Order/Enums/OrderStatusEnum.php
+++ app/Modules/Order/Enums/OrderStatusEnum.php
@@ -7,4 +7,14 @@ enum OrderStatusEnum: string
     case COMPLETED = 'completed';
     case IN_PROGRESS = 'in_progress';
     case WAITING_PAYMENT = 'waiting_payment';
+    case WARRANTY_PERIOD = 'warranty_period';
+
+    public static function forSeeder(): array
+    {
+        return $array = [
+            self::COMPLETED,
+            self::IN_PROGRESS,
+            self::WAITING_PAYMENT,
+        ];
+    }
 }
